cb8e0aa954f550118697e48f12946cf991a5e523
Timourka <timourkot@gmail.com>
2025-12-02 00:50:34 +0400

    добавил пример светодиода

---
+++ Exemples/BasicLED/BasicLED.cs
@@ -0,0 +1,27 @@
+﻿using Models;
+using System.Drawing;
+
+namespace Exemples.BasicLED
+{
+    public class BasicLED : Device
+    {
+        public BasicLED(Point position) : base(
+            new List<Picture> { new Picture( new Image("./LEDImage.png"), position) },
+            new LEDBehavior(),
+            new List<Connector>
+            {
+                new Connector(
+                    "anod", "anod", 0,
+                    new Picture(new Image("./anodImage.png"), new Point(position.X, position.Y))
+                ),
+                new Connector(
+                    "catod", "catod", 0,
+                    new Picture(new Image("./catodImage.png"), new Point(position.X + 50, position.Y))
+                ),
+            }
+            )
+        {
+            // DONOTHING
+        }
+    }
+}
---
+++ Exemples/BasicLED/LEDBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Models;
+
+namespace Exemples.BasicLED
+{
+    internal class LEDBehavior : IBehaviorScript
+    {
+        public bool IsRunning { get; set; }
+        public Device? Device { get; set; }
+
+        private Image _shiningImage;
+        private Image _notShiningImage;
+
+        public LEDBehavior()
+        {
+            _shiningImage = Image("./shiningLEDImage.png");
+            _notShiningImage = Image("./LEDImage.png");
+        }
+
+        private void checkAndChange()
+        {
+            if (Device.connectors[0].curentVoltage - Device.connectors[1].curentVoltage > 3)
+            {
+                Device.presentation[0].image = _shiningImage;
+            }
+            else
+            {
+                Device.presentation[0].image = _notShiningImage;
+            }
+        }
+
+        public void Loop()
+        {
+            checkAndChange();
+        }
+
+        public void Setup()
+        {
+            checkAndChange();
+        }
+    }
+}
